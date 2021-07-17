28b843d6ffda24eba725ce74f85f41a5ed448289
MR-JINN-OF-TG <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-07-17 12:31:24 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -1,25 +1,33 @@
-from pyrogram import filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import Filters, InlineKeyboardMarkup, InlineKeyboardButton
 
+from ..config import Config
 from ..screenshotbot import ScreenShotBot
 
 
-@ScreenShotBot.on_message(filters.private & filters.command("start"))
+@ScreenShotBot.on_message(Filters.private & Filters.command("start"))
 async def start(c, m):
-
+    
+    if not await c.db.is_user_exist(m.chat.id):
+        await c.db.add_user(m.chat.id)
+        await c.send_message(
+            Config.LOG_CHANNEL,
+            f"New User [{m.from_user.first_name}](tg://user?id={m.chat.id}) started."
+        )
+    
     await m.reply_text(
-        text=f"Hi there {m.from_user.mention}.\n\nI'm Screenshot Generator Bot. I can provide screenshots from "
-        "your video files without downloading the entire file (almost instantly). For more details check /help.",
+        text=f"Hi there {m.from_user.first_name}.\n\nI'm Screenshot Generator Bot. I can provide screenshots from your video files with out downloading the entire file (almost instantly). For more details check /help.",
         quote=True,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(
-                        "PROJECT GROUP", url="https://t.me/Nazriyaofftopic"
-                    ),
-                    InlineKeyboardButton("Project Channel", url="https://t.me/Nazriya_offtopic"),
+                    InlineKeyboardButton('SUPPORT GROUP', url='https://t.me/NAZRIYAOFFTOPIC'),
+                    InlineKeyboardButton('UPDATES CHANNEL', url='https://t.me/NAZRIYA_OFFTOPIC')
                 ],
-                [InlineKeyboardButton("My Father", url="https://t.me/smile_killer_010")],
+                [
+                    InlineKeyboardButton('SOURCE CODE', url='https://t.me/MR_JINN_OF_TG/16')
+                ]
             ]
-        ),
+        )
     )
+                    
+                
